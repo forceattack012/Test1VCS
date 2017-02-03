588d7bb34a33885f0c38829e159ef1a0b0d89c15
Forceattack012 <wisarutkim012@gmail.com>
2017-02-03 02:59:40 +0000

    -First version

--- src/generate_list.py
+++ src/generate_list.py
@@ -1,6 +1,7 @@
 import random
 def generate_list():
     alist = [x for x in range(random.randint(-10,10))]
+    #assert(alist)
     return alist
     
 """
--- src/genlist_test.py
+++ src/genlist_test.py
@@ -2,11 +2,11 @@
 
 import sys
 import os
-import generate_list.py
-cwd = os.getcwd() 
+
+cwd = os.getcwd() #Current Working Directory
 sys.path.append(cwd)
 # print(sys.path)
 
 # Test the module :generate_list
-from generate_list import printIt()
+from generate_list import printIt
 printIt()
