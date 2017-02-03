413fa79d6644e33e0b32db818426788c640b14c7
Forceattack012 <wisarutkim012@gmail.com>
2017-02-03 02:18:35 +0000

    First commit

---
+++ src/generate_list.py
@@ -0,0 +1,24 @@
+import random
+def generate_list():
+    alist = [x for x in range(random.randint(-10,10))]
+    return alist
+    
+"""
+print a generate_list
+"""
+
+def printIt() :
+    print( generate_list() )
+
+
+def main() :
+    printIt()
+    
+"""
+if this script flie is called, it will run main() directly
+"""
+
+if __name__ == '_main_' :
+    print("Test printIt() :")
+    main()
+    
